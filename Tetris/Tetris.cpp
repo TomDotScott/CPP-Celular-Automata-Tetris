4ec4711fcf3f74b8bc9e0ce6eb69fb37c0dee291
Tom Scott <tom.scott45456@gmail.com>
2024-04-07 22:01:18 +0100

    Draw tetrominoes to the screen
    The 7 types of tetromino are blitted to the pixel array

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,29 +1,115 @@
 #include <iostream>
+#include <random>
 #include <SFML/Graphics.hpp>
+
+#include "Tetromino.h"
+
+int RandomMultiple(const int multiple, const int max)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dist(0, max / multiple);
+
+	return dist(gen) * multiple;
+}
+
+void BlitPixels(uint8_t* pixels, const uint32_t* block, const sf::Vector2i& position, const int pixelsWidth)
+{
+	// Loop through each pixel block in the block
+	for (int blockY = 0; blockY < 2; ++blockY)
+	{
+		for (int blockX = 0; blockX < 4; ++blockX)
+		{
+			constexpr int blockSize = 10;
+			const int blockIndex = blockY * 4 + blockX;
+
+			const int pixelsIndex = ((position.y + blockY * blockSize) * pixelsWidth + position.x + blockX * blockSize) * 4;
+
+			const uint32_t color = block[blockIndex];
+
+			// Extract color channels
+			const uint8_t red = color >> 24 & 0xFF;
+			const uint8_t green = color >> 16 & 0xFF;
+			const uint8_t blue = color >> 4 & 0xFF;
+			const uint8_t alpha = color & 0xFF;
+
+			if (color > 0)
+			{
+				// Blit the color to the pixels array for each pixel in the block
+				for (int y = 0; y < blockSize; ++y)
+				{
+					for (int x = 0; x < blockSize; ++x)
+					{
+						const int pixelIndex = pixelsIndex + (y * pixelsWidth + x) * 4;
+						pixels[pixelIndex] = red;
+						pixels[pixelIndex + 1] = green;
+						pixels[pixelIndex + 2] = blue;
+						pixels[pixelIndex + 3] = alpha;
+					}
+				}
+			}
+		}
+	}
+}
+
+void GenerateTetrominoes(std::vector<Tetromino*>& tetrominoes)
+{
+	for (int i = 0; i < 49; ++i)
+	{
+		switch (i % 7)
+		{
+		case 0:
+			tetrominoes.emplace_back(new LBlock());
+			break;
+		case 1:
+			tetrominoes.emplace_back(new JBlock());
+			break;
+		case 2:
+			tetrominoes.emplace_back(new IBlock());
+			break;
+		case 3:
+			tetrominoes.emplace_back(new SBlock());
+			break;
+		case 4:
+			tetrominoes.emplace_back(new ZBlock());
+			break;
+		case 5:
+			tetrominoes.emplace_back(new TBlock());
+			break;
+		case 6:
+			tetrominoes.emplace_back(new OBlock());
+			break;
+		}
+	}
+}
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Cellular Automata Tetris");
+	static constexpr unsigned SCREEN_WIDTH = 800;
+	static constexpr unsigned SCREEN_HEIGHT = 600;
 
-	static constexpr unsigned W = 800;
-	static constexpr unsigned H = 600;
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Cellular Automata Tetris");
+	window.setFramerateLimit(60);
 
-	sf::Uint8* pixels = new sf::Uint8[W * H * sizeof(unsigned)];
+	auto* pixels = new sf::Uint8[static_cast<size_t>(SCREEN_WIDTH) * static_cast<size_t>(SCREEN_HEIGHT) * 4];
 
 	sf::Texture texture;
-	texture.create(W, H);
-
-	for (register int i = 0; i < (W * H * 4); i += 4)
-	{
-		pixels[i] = 255;
-		pixels[i + 1] = 0;
-		pixels[i + 2] = 0;
-		pixels[i + 3] = 255;
-	}
-
-	texture.update(pixels);
+	texture.create(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	sf::Sprite sprite(texture);
+
+	std::vector<Tetromino*> blocks;
+
+	GenerateTetrominoes(blocks);
+
+	for (auto* block : blocks)
+	{
+		const int randomX = RandomMultiple(10, 400);
+		const int randomY = RandomMultiple(10, 400);
+
+		block->SetPosition({ randomX, randomY});
+	}
 
 	while (window.isOpen())
 	{
@@ -37,6 +123,17 @@ int main()
 		}
 
 		window.clear(sf::Color::Black);
+
+		// Clear the pixel buffer
+		memset(pixels, 0, static_cast<size_t>(SCREEN_WIDTH) * static_cast<size_t>(SCREEN_HEIGHT) * 4);
+
+		// Update the pixels
+		for (auto& block : blocks)
+		{
+			BlitPixels(pixels, block->GetBuffer(), block->GetPosition(), SCREEN_WIDTH);
+		}
+
+		texture.update(pixels);
 
 		window.draw(sprite);
 
---
+++ Tetris/Tetromino.h
@@ -0,0 +1,135 @@
+#pragma once
+#include <SFML/Config.hpp>
+#include <SFML/System/Vector2.hpp>
+
+class Tetromino
+{
+public:
+	static constexpr unsigned TETROMINO_PIXEL_WIDTH = 4;
+	static constexpr unsigned TETROMINO_PIXEL_HEIGHT = 2;
+
+	#define SPRITE sf::Uint32 m_shape[TETROMINO_PIXEL_WIDTH * TETROMINO_PIXEL_HEIGHT]
+
+	const sf::Uint32* GetBuffer() const { return m_buffer; }
+
+	const sf::Vector2i& GetPosition() const { return m_position; }
+	void SetPosition(const sf::Vector2i& position) { m_position = position; }
+
+protected:
+	sf::Uint32* m_buffer = nullptr;
+
+private:
+	sf::Vector2i m_position;
+};
+
+class LBlock final : public Tetromino
+{
+public:
+	LBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0x00000000, 0x00000000, 0x00000000, 0xFFA500FF,
+		0xFFA500FF, 0xFFA500FF, 0xFFA500FF, 0xFFA500FF
+	};
+};
+
+class JBlock final : public Tetromino
+{
+public:
+	JBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0xADD8E6FF, 0x00000000, 0x00000000, 0x00000000,
+		0xADD8E6FF, 0xADD8E6FF, 0xADD8E6FF, 0xADD8E6FF
+	};
+};
+
+class IBlock final : public Tetromino
+{
+public:
+	IBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0x00008BFF, 0x00000000, 0x00000000, 0x00000000,
+		0x00008BFF, 0x00008BFF, 0x00008BFF, 0x00008BFF
+	};
+};
+
+class OBlock final : public Tetromino
+{
+public:
+	OBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0xFFFF00FF, 0xFFFF00FF, 0x00000000, 0x00000000,
+		0xFFFF00FF, 0xFFFF00FF, 0x00000000, 0x00000000
+	};
+};
+
+class SBlock final : public Tetromino
+{
+public:
+	SBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0x00000000, 0x90EE90FF, 0x90EE90FF, 0x00000000,
+		0x90EE90FF, 0x90EE90FF, 0x00000000, 0x00000000
+	};
+};
+
+class TBlock final : public Tetromino
+{
+public:
+	TBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0x00000000, 0xFF00FFFF, 0x00000000, 0x00000000,
+		0xFF00FFFF, 0xFF00FFFF, 0xFF00FFFF, 0x00000000
+	};
+};
+
+class ZBlock final : public Tetromino
+{
+public:
+	ZBlock()
+	{
+		m_buffer = m_shape;
+	}
+
+private:
+	SPRITE
+	{
+		0xBF0000FF, 0xBF0000FF, 0x00000000, 0x00000000,
+		0x00000000, 0xBF0000FF, 0xBF0000FF, 0x00000000
+	};
+};
