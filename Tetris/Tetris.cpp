4cfb082790cbb797c1d083a3794fc2b0f86e88f6
Tom Scott <tom.scott45456@gmail.com>
2024-04-07 14:29:19 +0100

    Create pixel array
    Draw red to the screen

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -3,24 +3,45 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Cellular Automata Tetris");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Cellular Automata Tetris");
 
-    std::cout << "Hello, World!\n";
+	static constexpr unsigned W = 800;
+	static constexpr unsigned H = 600;
 
-    while (window.isOpen())
-    {
-        sf::Event event{};
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+	sf::Uint8* pixels = new sf::Uint8[W * H * sizeof(unsigned)];
 
-        window.clear(sf::Color::Black);
-        window.display();
-    }
+	sf::Texture texture;
+	texture.create(W, H);
 
-    return 0;
+	for (register int i = 0; i < (W * H * 4); i += 4)
+	{
+		pixels[i] = 255;
+		pixels[i + 1] = 0;
+		pixels[i + 2] = 0;
+		pixels[i + 3] = 255;
+	}
+
+	texture.update(pixels);
+
+	sf::Sprite sprite(texture);
+
+	while (window.isOpen())
+	{
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear(sf::Color::Black);
+
+		window.draw(sprite);
+
+		window.display();
+	}
+
+	return 0;
 }
