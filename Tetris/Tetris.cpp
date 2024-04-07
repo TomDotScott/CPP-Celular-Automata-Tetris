9f01794b726841f07b6130f439349a4f92c0823c
Tom Scott <tom.scott45456@gmail.com>
2024-04-07 14:19:14 +0100

    Initial project
    Hello world SFML window

---
+++ Tetris/Tetris.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Cellular Automata Tetris");
+
+    std::cout << "Hello, World!\n";
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
